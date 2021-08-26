ffe4d13a5e78145c04866f43400d52f099c025a1
the-new-day <balakine977@yandex.ru>
2021-08-26 17:20:30 +0500

    2.4 done

---
+++ Chapter 2/2.4.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons 1 3)) ; 1
+(cdr (cons 1 3)) ; 3
+
