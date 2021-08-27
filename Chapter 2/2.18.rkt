6733d95c9633adab49e6266a1f9415ba6496419f
the-new-day <balakine977@yandex.ru>
2021-08-27 20:34:57 +0500

    2.18 - another implementation

--- Chapter 2/2.18.rkt
+++ Chapter 2/2.18.rkt
@@ -1,12 +1,11 @@
 #lang racket
 
 (define (reverse x)
-  (define (iter result cnt)
-    (if (< cnt 0)
+  (define (iter items result)
+    (if (null? items)
         result
-        (iter (append result
-                      (list (list-ref x cnt)))
-              (- cnt 1))))
-  (iter (list) (- (length x) 1)))
+        (iter (cdr items)
+              (cons (car items) result))))
+  (iter x '()))
 
 (reverse (list 1 4 9 16 25))
