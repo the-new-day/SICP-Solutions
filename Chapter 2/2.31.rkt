6124da59b891b1c23cec7cf79f418329dc153f60
the-new-day <balakine977@yandex.ru>
2021-08-28 18:03:40 +0500

    2.31 done

---
+++ Chapter 2/2.31.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (tree-map proc tree)
+  (map (lambda(sub-tree)
+         (if (pair? sub-tree)
+             (tree-map proc sub-tree)
+             (proc sub-tree)))
+       tree))
+
+(define (square-tree tree)
+  (tree-map sqr tree))
+
+(square-tree (list 1
+                   (list 2 (list 3 4) 5)
+                   (list 6 7)))
