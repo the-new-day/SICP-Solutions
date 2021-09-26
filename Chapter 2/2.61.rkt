a59dcc6b3d1dbacf6a1875cc66272c31a7f1bfea
the-new-day <balakine977@yandex.ru>
2021-09-26 12:28:19 +0500

    2.61 done

---
+++ Chapter 2/2.61.rkt
@@ -0,0 +1,16 @@
+#lang sicp
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< x (car set)) (cons x set))
+        ((= x (car set)) set)
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(define set (list 1 2 3 5))
+(adjoin-set 4 set)
